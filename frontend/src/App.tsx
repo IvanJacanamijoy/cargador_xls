55ad2b73d484cfd8e23d409d0ee232925b095986
ivan jacanamijoy <rojasjacanamijoyivan@gmail.com>
2025-10-16 07:28:09 -0500

    agregando estructura inicial del frontedn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <p className='text-red-500'>Probando tailwind</p>
+    </div>
+  )
+}
+
+export default App
